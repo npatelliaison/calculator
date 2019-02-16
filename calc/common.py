4d4ae194c457cfa66ead2b566c56aef407f991ef
Nital Patel <npatel@liaison.com>
2019-02-16 10:13:16 -0500

    Added two 2 new methods

--- calc/common.py
+++ calc/common.py
@@ -7,5 +7,15 @@ def sub(x, y):
     subtract = x - y
     print("Subtraction of two number is: ", str(subtract))
 
+def multiply(x, y):
+    mult = x * y
+    print("Multiplication of two numbers is: ", str(mult))
+
+def divide(x, y):
+    div = x / y
+    print("Division of two numbers i: ", str(div))
+
 add(4, 5)
 sub(10, 4)
+multiply(10, 10)
+divide(40, 10)
