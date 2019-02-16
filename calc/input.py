a85936fe85c5ff5a87b2827bc8073d07259c0ec5
Nital Patel <npatel@liaison.com>
2019-02-16 13:25:51 -0500

    Added conditional statement

--- calc/input.py
+++ calc/input.py
@@ -1,6 +1,24 @@
-from common import add
+from common import *
 
 x = int(input("Please enter First numbers: "))
 y = int(input("Please enter second number: "))
+z = int(input("Which operation: "))
 
-add(x, y)
+if (z == 1):
+    add(x, y)
+elif (z == 2):
+    sub(x, y)
+elif (z == 3):
+    multiply(x, y)
+else:
+    divide(x, y)
+
+'''
+# def math(z):
+options =  {
+            1: add(x, y),
+            2: sub(x, y),
+            3: multiply(x, y),
+            4: divide(40, 10),
+            }
+'''
