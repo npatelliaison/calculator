77e56924a276953e053786e08926a3bf0e2b179b
Nital Patel <npatel@liaison.com>
2019-02-18 20:27:56 -0500

    Adding numbers

--- calc/basic.py
+++ calc/basic.py
@@ -1,32 +1,32 @@
 
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SubmitField
-# from common import *
+from wtforms import IntegerField, SubmitField, FloatField, StringField
+from common import add
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'calculatorkey'
 
 class infoForm(FlaskForm):
-    number1 = StringField("First Number: ")
-    number2 = StringField("Second Number: ")
+    number1 = IntegerField("First Number: ")
+    number2 = IntegerField("Second Number: ")
     submit = SubmitField("Submit")
-    result = StringField("Result: ")
+    n3 = IntegerField("Result: ")
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     
     n1 = False
     n2 = False
-    result = False
+    
     form = infoForm()
     if  form.validate_on_submit():
         session['n1'] = form.number1.data
         session['n2'] = form.number2.data
         session['n3'] = form.number1.data + form.number2.data
-
-        return redirect(url_for("result"))
+ #       result = add(n1, n2)
+        return redirect(url_for('result'))
 
     return render_template('index.html',form=form)
 
